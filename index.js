d3a45f659298f833373f250e9481a1a36a530595
stur86 <simonesturniolo@gmail.com>
2019-09-01 18:34:47 +0100

    Tests with Vue

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var $ = require('jquery');
-var Vue = require('vue').default;
-
 $(document).ready(function() {
     var app = new Vue({
         el: '#app',
@@ -9,5 +6,34 @@ $(document).ready(function() {
         }
     });
 
-    console.log(app);
+    var app2 = new Vue({
+        el: '#app-2',
+        data: {
+          message: 'You loaded this page on ' + new Date().toLocaleString(),
+          myclass: 'lolno',
+          isticked: true
+        },
+        methods: {
+            changeClass: function() {
+                if (this.myclass == 'lolno') {
+                    this.myclass = '';
+                }
+                else {
+                    this.myclass = 'lolno';
+                }
+            }
+        }
+    });
+
+    var app3 = new Vue({
+        el: '#app-3',
+        data: {
+            zoom: 10,
+            segments: [{'w': 10}, {'w': 5}, {'w': 3}],
+        }
+    })
+
+    window.app = app;
 });
+
+
